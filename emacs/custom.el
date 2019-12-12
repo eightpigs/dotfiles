5b382e335377d2b1be0d319d59d2df2b11100b48
eightpigs <eightpigs@outlook.com>
2019-12-12 14:16:33 +0800

    将emacs默认的custom写入custom中；调整org-mode配置

---
+++ emacs/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(horizontal-scroll-bar-mode nil)
+ '(markdown-command "pandoc --mathml --quiet")
+ '(markdown-enable-math t)
+ '(markdown-xhtml-header-content "
+<meta name=\"viewport\" content=\"width=device-width\">")
+ '(scroll-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:background nil)))))
