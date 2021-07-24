629159741aab477b17180528d990a1307eb0ea59
eightpigs <eightpigs@outlook.com>
2021-07-24 23:07:14 +0800

    fix the circular reference problem of x11, xmodmap; remove tmux/setup.sh

--- cfg/gtk-3.0/.init.sh
+++ cfg/gtk-3.0/.init.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-whereis gtk-config > /dev/null 2>&1 && ln -sfv $PWD/cfg/gtk-3.0 ~/.gtk-3.0
+whereis gtk-config > /dev/null 2>&1 && \
+  (rm -rf $HOME/.gtk-3.0 && ln -sfv $PWD/cfg/gtk-3.0 $HOME/.gtk-3.0)
--- cfg/x11/.init.sh
+++ cfg/x11/.init.sh
@@ -2,6 +2,8 @@
 
 # x11, wayland, tty
 if [[ $XDG_SESSION_TYPE == "x11" ]]; then
+  rm ~/.Xresources
+  rm ~/.xinitrc
   ln -sfv $PWD/cfg/x11/Xresources ~/.Xresources
   ln -sfv $PWD/cfg/x11/xinitrc ~/.xinitrc
   xrdb -merge $HOME/.Xresources
