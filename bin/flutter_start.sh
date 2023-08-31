a3da961394fde7d443bc72e115c87e5ec0db144a
eightpigs <eightpigs@outlook.com>
2023-08-31 16:48:41 +0800

    flutter_start.sh support get device from args

--- bin/flutter_start.sh
+++ bin/flutter_start.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-# 
-devices=$(flutter devices --machine)
+if [[ ! -z $1 ]]; then
+  flutter run --debug -d "$1"
+else
+  # 
+  devices=$(flutter devices --machine)
 
-#  jq 
-device_ids=$(echo "$devices" | jq -r '.[].id')
+  #  jq 
+  device_ids=$(echo "$devices" | jq -r '.[].id')
 
-#  id
-device_array=()
-for device in $device_ids
-do
-  device_array+=("$device")
-done
+  #  id
+  device_array=()
+  for device in $device_ids
+  do
+    device_array+=("$device")
+  done
 
-# 
-echo "Please select a device:"
-for i in "${!device_array[@]}"; do
-  printf "%d) %s\n" $((i+1)) "${device_array[$i]}"
-done
+  # 
+  echo "Please select a device:"
+  for i in "${!device_array[@]}"; do
+    printf "%d) %s\n" $((i+1)) "${device_array[$i]}"
+  done
 
-# 
-read -p "Enter the number of your choice: " choice
+  # 
+  read -p "Enter the number of your choice: " choice
 
-#  id
-selected_device=${device_array[$((choice-1))]}
+  #  id
+  selected_device=${device_array[$((choice-1))]}
 
-#  id  flutter 
-flutter run --debug -d "$selected_device"
+  #  id  flutter 
+  flutter run --debug -d "$selected_device"
+fi
